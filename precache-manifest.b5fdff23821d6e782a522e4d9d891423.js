650ea24f75555a17efa6ab69a1aa6f67887334fb
Keikeu <karolina.m.placek@gmail.com>
2019-10-16 12:26:40 +0200

    Updates

---
+++ precache-manifest.b5fdff23821d6e782a522e4d9d891423.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de54b5fcee0473b2faf2853f977db0e",
+    "url": "/transactions-crud/index.html"
+  },
+  {
+    "revision": "7ae67fd0f48deec8d074",
+    "url": "/transactions-crud/static/css/main.38430af3.chunk.css"
+  },
+  {
+    "revision": "bf173d575f80e7ac9e83",
+    "url": "/transactions-crud/static/js/2.c020fb1d.chunk.js"
+  },
+  {
+    "revision": "7ae67fd0f48deec8d074",
+    "url": "/transactions-crud/static/js/main.72cb35e7.chunk.js"
+  },
+  {
+    "revision": "5af45cc4ce09b3c52e77",
+    "url": "/transactions-crud/static/js/runtime-main.9a9fdc7e.js"
+  }
+]);
